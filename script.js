973eeb8987656fb91a728b0b9341c5813d173568
Daniel Hawthorne-Foss <dhfoss89@gmail.com>
2020-09-24 16:15:23 -0400

    connected my function to the button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function getPassword() {
+function generatePassword() {
     
     //1. All variables for the function
     var lowerCases = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -95,4 +95,17 @@ function getPassword() {
     return passwordString;
 }
 
-console.log(getPassword());
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
